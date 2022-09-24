d584e05ad0512c429bb6494bec52a98f8e23abd1
Maria Yutaeva <mariya1982@yandex.ru>
2022-09-24 13:46:14 +0300

    задача19

---
+++ Task19/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+int Input()
+{
+    Console.WriteLine("Введите пятизначное число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+void zadacha()
+{
+    int num = Input();
+    int one = num / 10000 % 10;
+    int two = num / 1000 % 10;
+    int three = num / 100 % 10;
+    int four = num / 10 % 10;
+    int five = num % 10;
+    if (one == five && two == four)
+    {
+        Console.WriteLine("Это число полиндром! ");
+    }
+    else 
+    {
+        Console.WriteLine("Это число НЕ полиндром! ");
+    }
+}
+zadacha();
