0c7bcf4c5a9f31d66a37ff1ab60c674198730e77
Maria Yutaeva <mariya1982@yandex.ru>
2022-09-24 15:46:34 +0300

    правки

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -16,16 +16,17 @@ void zadacha()
     int num = Input();
     int one = num / 10000 % 10;
     int two = num / 1000 % 10;
-    int three = num / 100 % 10;
     int four = num / 10 % 10;
     int five = num % 10;
-    if (one == five && two == four)
-    {
-        Console.WriteLine("Это число палиндром! ");
+    if (num > 9999 && num < 99999)
+    { 
+       if (one == five && two == four)
+            Console.WriteLine("Это число палиндром! ");
+       else 
+           Console.WriteLine("Это число НЕ палиндром! ");
     }
     else 
-    {
-        Console.WriteLine("Это число НЕ палиндром! ");
-    }
+        Console.WriteLine("Вы ввели не пятизначное число! ");
+     
 }
 zadacha();
